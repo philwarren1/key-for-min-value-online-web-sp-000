bf8d531ee92244e3a4c2d65abc8a2eb5e191e4cb
Philip Warren <philwarren1@gmail.com>
2020-07-30 16:09:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
- new_hash = []
- 
+ name_hash = []
+ name_hash.collect do |key, value|
+    key
+ end
 end
