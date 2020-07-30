2b78204ad2d11b2c832c42c5278eb208caa86ecd
Philip Warren <philwarren1@gmail.com>
2020-07-30 16:17:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,13 @@
 
 def key_for_min_value(name_hash)
    lvalue = 0
-   key = nil
+   lkey = nil
   
   name_hash.each do |key,value|
-    if lowest_value == 0 || value < lowest_value
-      lowest_value = value 
-      lowest_key = key
+    if lvalue == 0 || value < lvalue
+      lvalue = value 
+      lkey = key
     end
   end
-  lowest_key
+  lkey
 end
